c33758675c6f82813dc087d3bbb5e4b91027e3b5
aziz-ben-cheikh <azizbencheikh1@gmail.com>
2025-04-23 18:58:09 +0100

    updated the user routes to use authorize permission instead of authorize role

--- src/auth.js
+++ src/auth.js
@@ -6,20 +6,16 @@ export function authenticateToken(req, res, next) {
     const authHeader = req.headers["authorization"];
     const token = authHeader && authHeader.split(" ")[1];
 
-    console.log("Auth Header:", authHeader);
-    console.log("Token Extracted:", token);
+
 
     if (!token) {
-        console.log("No token provided");
         return res.sendStatus(401);
     }
 
     jwt.verify(token, JWT_SECRET, (err, user) => {
         if (err) {
-            console.log("JWT verification failed:", err.message);
             return res.sendStatus(403);
         }
-        console.log("JWT Decoded User:", user);
         req.user = user;
         next();
     });
--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -5,10 +5,10 @@ import { authenticateToken,authorizePermission } from "../auth.js";
  const router = express.Router();
  
  router.post("/login", userController.login.bind(userController));
- router.post("/", userController.createuser);
- router.get("/", userController.getalluser);
- router.get("/:id", userController.getuserbyid);
- router.put("/:id", userController.updateuser);
+ router.post("/",  authenticateToken, authorizePermission("create_users"), userController.createuser);
+ router.get("/", authenticateToken, authorizePermission("read_users"), userController.getalluser);
+ router.get("/:id",authenticateToken, authorizePermission("read_users"), userController.getuserbyid);
+ router.put("/:id",authenticateToken, authorizePermission("update_users"), userController.updateuser);
  router.delete("/:id", authenticateToken, authorizePermission("delete_users"), userController.deleteuser);
 
  export default router;
